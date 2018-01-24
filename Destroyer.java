837a18bc96fde8a8ed057ecf9fc763bf5145b15b
MWintz <35759895+MWintz@users.noreply.github.com>
2018-01-24 21:06:59 +0100

    Update Destroyer.java

--- Destroyer.java
+++ Destroyer.java
@@ -1,5 +1,9 @@
+package units;
+
 import java.util.ArrayList;
 import datas.Resources;
+import datas.Stats;
+import datas.Position;
 
 public class Destroyer extends Naval {
 	
@@ -8,18 +12,19 @@ public class Destroyer extends Naval {
 	private static final RANGE = 1;
 	private static final ATTACK = 10;
 	private static final DEFENSE = 6;
-	private static final int MONEY_UPKEEP = 0;
-	private static final int FOOD_UPKEEPER = 0;
-	private static final int OIL_UPKEEPER = 0;
-	private static final int ELECTRICITY_UPKEEPER = 0;
 	
-	public Destroyer(Stats stats int vie, Position position, int faction, float movement, int range, Resources upkeep, ArrayList<String> crossable) {
-		super(stats, position, faction, movement, range, cost, upkeep, crossable);
+	private static final int MONEY_UPKEEP = 0;
+	private static final int FOOD_UPKEEP = 0;
+	private static final int OIL_UPKEEP = 0;
+	private static final int ELECTRICITY_UPKEEP = 0;
+	
+	public Destroyer(Stats stats, int vie, Position position, int faction, float movement, int range, Resources upkeep) {
+		super(stats, position, faction, movement, range, cost, upkeep);
 	}
 	
 	
-	public Destroyer(Position position, int faction, ArrayList<String> crossable) {
-		this(new Stats(MAX_HEALTH_POINTS, ATTACK, DEFENSE), position, faction, MOVEMENT, RANGE, new Ressources(MONEY_UPKEEP, FOOD_UPKEEPER, OIL_UPKEEPER, ELECTRICITY_UPKEEPER), null);
+	public Destroyer(Position position, int faction) {
+		this(new Stats(MAX_HEALTH_POINTS, ATTACK, DEFENSE), position, faction, MOVEMENT, RANGE, new Ressources(MONEY_UPKEEP, FOOD_UPKEEPER, OIL_UPKEEPER, ELECTRICITY_UPKEEPER));
 	}
 	
 }
