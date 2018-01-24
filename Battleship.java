475beb988d16d84d9e60ce473f48e862ff70b282
MWintz <35759895+MWintz@users.noreply.github.com>
2018-01-24 21:08:25 +0100

    Update Battleship.java

--- Battleship.java
+++ Battleship.java
@@ -17,12 +17,12 @@ public class Battleship extends Naval{
 	private static final int OIL_UPKEEP = 0;
 	private static final int ELECTRICTY_UPKEEP = 0;
 	
-	public Battleship(Stats stats, Position position, int faction, float movement, int range, Resources upkeep, ArrayList<String> crossable) {
-		super(stats, position, faction, movement, range, cost, upkeep, crossable);
+	public Battleship(Stats stats, Position position, int faction, float movement, int range, Resources upkeep) {
+		super(stats, position, faction, movement, range, cost, upkeep);
 	}
 	
 	
-	public Battleship(Position position, int faction, ArrayList<String> crossable) {
-		this(new Stats(MAX_HEALTH_POINT,ATTACK,DEFENSE),position,faction,MOVEMENT,RANGE,new Resources(MONEY_UPKEEP,FOOD_UPKEEP,OIL_UPKEEP,ELECTRICTY_UPKEEP),crossable);
+	public Battleship(Position position, int faction) {
+		this(new Stats(MAX_HEALTH_POINT,ATTACK,DEFENSE),position,faction,MOVEMENT,RANGE,new Resources(MONEY_UPKEEP,FOOD_UPKEEP,OIL_UPKEEP,ELECTRICTY_UPKEEP));
 	}
 }
