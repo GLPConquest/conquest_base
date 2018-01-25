4fbc23f9e12a8e145e30621f723225c6b0fb1ff2
MWintz <35759895+MWintz@users.noreply.github.com>
2018-01-25 22:03:06 +0100

    Update Naval.java

--- Naval.java
+++ Naval.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
+
+
 public abstract class Naval extends Unit {
 
-	public Naval(int vie, Position pos, int defense, String faction, int movement, int range, Cost cost, Cost upkeep, String[] way) {
-		super(vie, pos, defense, faction, movement, range, cost, upkeep, way);
+	private ArrayList<String> crossable = new ArrayList<String>();
+	
+	public Naval(int vie, Position pos, int defense, String faction, int movement, int range, Cost cost, Cost upkeep) {
+		setDefinedCrossable();
+		super(vie, pos, defense, faction, movement, range, cost, upkeep, getCrossable());
 	}
 	public Naval() {
-		this(100,new Position(),0,"neutral",0,0,new Cost(),new Cost(),null);
+		setDefinedCrossable();
+		this(100,new Position(),0,"neutral",0,0,new Cost(),new Cost(), getCrossable());
+	}
+	public void setDefinedCrossable(){
+		this.crossable=this.crossable.add("water");
+	}
+	public ArrayList<String> getCrossable(){
+		return this.crossable;
 	}
 }
