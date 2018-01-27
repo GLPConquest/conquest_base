cfd25930a97c0b3807d67e1f0cfae5604dba8ee9
MWintz <35759895+MWintz@users.noreply.github.com>
2018-01-27 19:27:10 +0100

    Update Unit.java

--- Unit.java
+++ Unit.java
@@ -1,89 +1,117 @@
+package units;
 
+import java.util.ArrayList;
+import datas.Resources;
+import datas.Stats;
+import datas.Position;
+
+/*
+* This class is used to init every unit
+* The global construction of a unit is defined here
+*/
 public abstract class Unit {
+	/*Units have stats...*/
+	private Stats stats;
+	/*...a position...*/
+	private Position position;
+	/*...a faction...*/
+	private int faction;
+	/*...a movement range...*/
+	private float movement;
+	/*...an attack range...*/
+	private int range;
+	/*...an upkeep cost...*/
+	private Resources upkeep;
+	/*... and they can go through some different type of square*/
+	private ArrayList<String> crossable;
 	
+	/*
+	* Custom Constructor 
+	*/
+	public Unit(Stats stats, Position position, int faction, float movement, int range, Resources upkeep, ArrayList<String> crossable) {
+		setStats(stats);
+		setPosition(position);
+		setFaction(faction);
+		setMovement(movement);
+		setRange(range);
+		setUpkeep(upkeep);
+		setCrossable(crossable);
+	}
+	
+	/*
+	* Default constructor
+	*/
 	public Unit() {
-		
+		this(getStats(),getPosition(),getFaction(),getFaction(),getMovement(),getRange(),getUpkeep(),getCrossable());
 	}
 	
-	public Unit() {
-		this(100,new Position(),0,"neutral",0,0,new Cost(),new Cost(),null);
+	/*Stats getter*/
+	public Stats getStats() {
+		return this.stats;
 	}
 
-	public int getVie() {
-		return vie;
+	/*Stats setter*/
+	public void setStats(Stats stats) {
+		this.stats = stats;
 	}
 
-	public void setVie(int vie) {
-		this.vie = vie;
+	/*Position getter*/
+	public Position getPosition() {
+		return this.position;
 	}
 
-	public Position getPos() {
-		return pos;
+	/*Position setter*/
+	public void setPosition(Position position) {
+		this.position = position;
 	}
 
-	public void setPos(Position pos) {
-		this.pos = pos;
-	}
-	
-	public static int getAttaque() {
-		return attaque;
+	/*Faction getter*/
+	public int getFaction() {
+		return this.faction;
 	}
 
-	public int getDefense() {
-		return defense;
-	}
-
-	public void setDefense(int defense) {
-		this.defense = defense;
-	}
-
-	public String getFaction() {
-		return faction;
-	}
-
-	public void setFaction(String faction) {
+	/*Faction setter*/
+	public void setFaction(int faction) {
 		this.faction = faction;
 	}
 
-	public int getMovement() {
-		return movement;
+	/*Movement getter*/
+	public float getMovement() {
+		return this.movement;
 	}
 
-	public void setMovement(int movement) {
+	/*Movement setter*/
+	public void setMovement(float movement) {
 		this.movement = movement;
 	}
 
+	/*Range getter*/
 	public int getRange() {
-		return range;
+		return this.range;
 	}
 
+	/*Range setter*/
 	public void setRange(int range) {
 		this.range = range;
 	}
 
-	public Cost getCost() {
-		return cost;
+	/*Upkeep getter*/
+	public Resources getUpkeep() {
+		return this.upkeep;
 	}
 
-	public void setCost(Cost cost) {
-		this.cost = cost;
-	}
-
-	public Cost getUpkeep() {
-		return upkeep;
-	}
-
-	public void setUpkeep(Cost upkeep) {
+	/*Upkeep setter*/
+	public void setUpkeep(Resources upkeep) {
 		this.upkeep = upkeep;
 	}
 
-	public String[] getWay() {
-		return way;
+	/*Crossable getter*/
+	public ArrayList<String> getCrossable() {
+		return this.crossable;
 	}
 
-	public void setWay(String[] way) {
-		this.way = way;
-	}
-	
-	
+	/*Crossable setter*/
+	public void setCrossable(ArrayList<String> crossable) {
+		this.crossable = crossable;
+	}	
 }
