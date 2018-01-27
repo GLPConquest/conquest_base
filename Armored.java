ae22fc35bd99554e6f23f6f245dcc91b50013799
MWintz <35759895+MWintz@users.noreply.github.com>
2018-01-27 19:50:01 +0100

    Update Armored.java

--- Armored.java
+++ Armored.java
@@ -1,9 +1,30 @@
+package units;
 
+import java.util.ArrayList;
+import datas.Resources;
+import datas.Stats;
+import datas.Position;
+
+/*
+* This class is used to build the armored units
+* These units are strong against the infantry units and weak against bots units
+* Moreover these units are slower than the other
+*/
 public class Armored extends Earths{
-	public Armored(int vie, Position pos, int defense, String faction, int movement, int range, Cost cost, Cost upkeep, String[] way) {
-		super(vie, pos, defense, faction, movement, range, cost, upkeep, way);
+	
+	private String attribute = "Armored";
+	
+	
+	public Armored(Stats stats, Position position, int faction, float movement, int range, Resources upkeep, ArrayList<String> way) {
+		super(stats, position, faction, movement, range, upkeep, crossable, getAttribute());
 	}
+	
+	
 	public Armored() {
-		this(100,new Position(),0,"neutral",0,0,new Cost(),new Cost(),null);
+		this(getStats(), getPosition(), getFaction(), getMovement(), getRange(), getUpkeep(), getCrossable(), getAttribute());
 	} 
+	
+	public String getAttribute(){
+		return this.attribute;
+	}
 }
