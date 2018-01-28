ace70ba063f2254f9283fdea23cbb0c4ddab7abf
MWintz <35759895+MWintz@users.noreply.github.com>
2018-01-28 13:13:24 +0100

    Update Bots.java

--- Bots.java
+++ Bots.java
@@ -1,9 +1,51 @@
+package units;
 
+import java.util.ArrayList;
+import datas.Resources;
+import datas.Stats;
+import datas.Position;
+
+/*
+* This class is used to build Bots
+* The environment these units can go through is defined here
+* They arestrong against Armored units but weak to Infantry
+*/
 public class Bots extends Earths {
-	public Bots(int vie, Position pos, int defense, String faction, int movement, int range, Cost cost, Cost upkeep, String[] way) {
-		super(vie, pos, defense, faction, movement, range, cost, upkeep, way);
+	
+	/*
+	* Custom constructor
+	*/
+	public Bots(Stats stats, Position position, int faction, float movement, int range, Resources upkeep, AraryList<int> crossables) {
+		setCrossable(crossables);
+		super(stats, position, faction, movement, range, upkeep, getCrossable());
 	}
+	
+	/*
+	* Default constructor
+	*/
 	public Bots() {
-		this(100,new Position(),0,"neutral",0,0,new Cost(),new Cost(),null);
+		setDefaultAttribute();
+		setDefaultCrossable();
+		this(getStats(), getMaxHealth(), getCurrentHealth(), getPosition(), getFaction(), getMovement(), getRange(), getUpkeep(), getCrossable(), getAttribute());
+	}
+	
+	
+	public setDefaultAttribute(){
+		this.attribute = "Bots";
+	}
+	
+	/*
+	* Default settings for crossable path : Bots can go everywhere but not in water
+	*/
+	public void setDefaultCrossable(){
+		this.crossable=this.crossable.add(1);
+		this.crossable=this.crossable.add(2);
+		this.crossable=this.crossable.add(3);
+		this.crossable=this.crossable.add(4);
+		this.crossable=this.crossable.add(5);
+		this.crossable=this.crossable.add(6);
+		this.crossable=this.crossable.add(7);
+		this.crossable=this.crossable.add(8);
+		this.crossable=this.crossable.add(9);
 	}
 }
