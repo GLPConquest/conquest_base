81bfce4be4e258a57729e2c7f3275adb6ad3baf1
MWintz <35759895+MWintz@users.noreply.github.com>
2018-01-28 12:49:54 +0100

    Update Assault.java

--- Assault.java
+++ Assault.java
@@ -1,10 +1,45 @@
+package units;
 
+import java.util.ArrayList;
+import datas.Resources;
+import datas.Stats;
+import datas.Position;
+
+/*
+* This class is used to build the Assault unit, a basic soldier with an assault rifle
+* It has its own stats, position, faction, movement, range and Cost
+* The environment it can go through is defined in the parent class
+* It is strong against Bots but weak against Armored units
+*/
 public class Assault extends Infantry{
-	public Assault(int vie, Position pos, String faction, int movement, int range, Cost cost, Cost upkeep, String[] way) {
-		super(vie, pos, 5, faction, movement, range, cost, upkeep, way);
+	/*
+	* Stats definition
+	*/
+	private static final int MAX_HEALTH_POINTS = 100;
+	private static final float MOVEMENT = 3;
+	private static final int RANGE = 1;
+	private static final int ATTACK = 6;
+	private static final int DEFENSE = 4;
+	
+	/*
+	* Cost definition
+	*/
+	private static final int MONEY_UPKEEP = 0;
+	private static final int FOOD_UPKEEP = 0;
+	private static final int OIL_UPKEEP = 0;
+	private static final int ELECTRICTY_UPKEEP = 0;
+	
+	/*
+	* Custom constructor
+	*/
+	public Assault(Stats stats, float maxHealth, float currentHealth, Position position, int faction, float movement, int range, Resources upkeep) {
+		super(stats, maxHealth, maxHealth, position, faction, movement, range, cost, upkeep);
 	}
 	
-	public Assault() {
-		this(100,new Position(),"neutral",0,0,new Cost(),new Cost(),null);
+	/*
+	* Default constructor
+	*/
+	public Assault(Position position, int faction) {
+		this(new Stats(MAX_HEALTH_POINT,ATTACK,DEFENSE), (float)MAX_HEALT_POINTS, (float)MAX_HEALT_POINTS, position, faction, MOVEMENT, RANGE, new Resources(MONEY_UPKEEP,FOOD_UPKEEP,OIL_UPKEEP,ELECTRICTY_UPKEEP));
 	}	
 }
