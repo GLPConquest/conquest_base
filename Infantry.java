dea6b1b8fa49a6488276ba7835cc84b444eb9573
MWintz <35759895+MWintz@users.noreply.github.com>
2018-01-28 13:21:58 +0100

    Update Infantry.java

--- Infantry.java
+++ Infantry.java
@@ -1,9 +1,46 @@
+package units;
 
-public class Infantry extends Earths {
-	public Infantry(int vie, Position pos, int defense, String faction, int movement, int range, Cost cost, Cost upkeep, String[] way) {
-		super(vie, pos, defense, faction, movement, range, cost, upkeep, way);
+import java.util.ArrayList;
+import datas.Resources;
+import datas.Stats;
+import datas.Position;
+
+/*
+* This class is used to build the infantry units
+* The environment these units can go through is defined here
+* These units are strong against the Bots units and weak against Armored units
+*/
+public class Infantry extends Unit {
+	
+	/*
+	* Custom Constructor 
+	*/
+	public Infantry(Stats stats, float maxHealth, float currentHealth, Position position, int faction, float movement, int range, Resources upkeep, ArrayList<String> way) {
+		super(stats, maxHealth, currentHealth, position, faction, movement, range, upkeep, crossable, getAttribute());
 	}
+	
+	/*
+	* Default constructor
+	*/
 	public Infantry() {
-		this(100,new Position(),0,"neutral",0,0,new Cost(),new Cost(),null);
+		setDefaultAttribute();
+		setDefaultCrossable();
+		this(getStats(), getMaxHealth(), getCurrentHealth(), getPosition(), getFaction(), getMovement(), getRange(), getUpkeep(), getCrossable(), getAttribute());
+	} 
+	
+	public setDefaultAttribute(){
+		this.attribute = "Infantry";
+	}
+	
+	public setDefaultCrossable(){
+		this.crossable = this.crossable.add(1);
+		this.crossable = this.crossable.add(2);
+		this.crossable = this.crossable.add(3);
+		this.crossable = this.crossable.add(4);
+		this.crossable = this.crossable.add(5);
+		this.crossable = this.crossable.add(6);
+		this.crossable = this.crossable.add(7);
+		this.crossable = this.crossable.add(8);
+		this.crossable = this.crossable.add(9);
 	}
 }
