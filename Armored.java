29c6ac0b9281754dbf57b81b80c16b152f9129b5
MWintz <35759895+MWintz@users.noreply.github.com>
2018-01-28 13:25:19 +0100

    Update Armored.java

--- Armored.java
+++ Armored.java
@@ -15,7 +15,7 @@ public class Armored extends Unit{
 	/*
 	* Custom Constructor 
 	*/
-	public Armored(Stats stats, float maxHealth, float currentHealth, Position position, int faction, float movement, int range, Resources upkeep, ArrayList<String> way) {
+	public Armored(Stats stats, float maxHealth, float currentHealth, Position position, int faction, float movement, int range, Resources upkeep, ArrayList<int> crossable) {
 		super(stats, maxHealth, currentHealth, position, faction, movement, range, upkeep, crossable, getAttribute());
 	}
 	
