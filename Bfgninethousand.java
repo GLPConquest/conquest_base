c9788d7601eb4aba5dcd70031140609b7456b306
MWintz <35759895+MWintz@users.noreply.github.com>
2018-01-28 13:06:42 +0100

    Update Bfgninethousand.java

--- Bfgninethousand.java
+++ Bfgninethousand.java
@@ -1,10 +1,46 @@
+package units;
 
+import java.util.ArrayList;
+import datas.Resources;
+import datas.Stats;
+import datas.Position;
+
+/*
+* This class is used to build the BFG-9000 unit, a snake like ranged bot
+* It has its own stats, position, faction, movement, range and Cost
+* The environment it can go through is defined in the parent class
+* It is strong against Armored units but weak to the infantry
+*/
 public class Bfgninethousand extends Bots{
-	public Bfgninethousand(int vie, Position pos, String faction, int movement, int range, Cost cost, Cost upkeep, String[] way) {
-		super(vie, pos, 3, faction, movement, range, cost, upkeep, way);
+	
+	/*
+	* Stats definition
+	*/
+	private static final int MAX_HEALTH_POINTS = 100;
+	private static final float MOVEMENT = 3;
+	private static final int RANGE = 3;
+	private static final int ATTACK = 10;
+	private static final int DEFENSE = 4;
+	
+	/*
+	* Cost definition
+	*/
+	private static final int MONEY_UPKEEP = 0;
+	private static final int FOOD_UPKEEP = 0;
+	private static final int OIL_UPKEEP = 0;
+	private static final int ELECTRICTY_UPKEEP = 0;
+	
+	/*
+	* Custom constructor
+	*/
+	public Bfgninethousand(Stats stats, float maxHealth, float currentHealth, Position position, int faction, float movement, int range, Resources upkeep) {
+		super(stats, maxHealth, maxHealth, position, faction, movement, range, cost, upkeep);
 	}
 	
-	public Bfgninethousand() {
-		this(100,new Position(),"neutral",0,0,new Cost(),new Cost(),null);
+	/*
+	* Default constructor
+	*/
+	public Bfgninethousand(Position position, int faction) {
+		this(new Stats(MAX_HEALTH_POINT,ATTACK,DEFENSE), (float)MAX_HEALT_POINTS, (float)MAX_HEALT_POINTS, position, faction, MOVEMENT, RANGE, new Resources(MONEY_UPKEEP,FOOD_UPKEEP,OIL_UPKEEP,ELECTRICTY_UPKEEP));
 	}
 }
